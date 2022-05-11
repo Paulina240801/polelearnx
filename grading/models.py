5ad38ea5142ac937852faee3234e86859dfe181b
Paulina240801 <paulinakania@student.agh.edu.pl>
2022-05-12 01:30:17 +0200

    Add Teacher and Student models.

--- grading/models.py
+++ grading/models.py
@@ -1,12 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+
+class Student(User):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    is_student = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Teacher(User):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    is_teacher = models.BooleanField(default=True)
+    students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return self.user.username
 
 
 class TestTemplate(models.Model):
     name = models.TextField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 
 
 class Test(models.Model):
-    test = models.ForeignKey(TestTemplate, on_delete=models.CASCADE)
+    test_template = models.ForeignKey(TestTemplate, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Question(models.Model):
